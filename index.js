6509df1ab5f60b997c230e33fe1f418676576659
Billal Farabi <bfarabi108@gmail.com>
2021-08-06 16:48:12 +0600

    port and node

--- index.js
+++ index.js
@@ -11,7 +11,11 @@ app.use(express.json());
 app.use(cors());
 const port = 5000
 
-
+app.get('/', (req, res) => {
+    
+        res.send('heroku is working');
+    
+})
 
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
